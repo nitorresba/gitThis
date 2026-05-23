cfbebd140f9bdc48392460c35f0b4dbc89b54f29
Daniel Santiago Garcia Gonzalez <dgarciagon@unal.edu.co>
2026-05-23 16:15:54 -0500

    se agregaron lineas para realizar lectura de archivos

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
 int main(){
+	ifstream archivoEntrada("inputFile.txt");
 	
+	if(archivoEntrada.is_open()){
+       cout<<"archivo abierto "<< endl;
+       string linea;
+       
+       while(getline(archivoEntrada , linea )){
+       
+       
+                                    
+       }                                                          
+    }
 }
