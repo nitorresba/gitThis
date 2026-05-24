a3269cb0927a94f96adf25b739ac33a8184bd7da
Sr Aquio <dvillamils@unal.edu.co>
2026-05-24 11:28:19 -0500

    se agregó el ofstream, la carpeta output y la lectura del ifstream para escritura en ofstream

--- main.cpp
+++ main.cpp
@@ -3,16 +3,18 @@
 #include <sstream>
 using namespace std;
 int main(){
-	ifstream archivoEntrada("inputFile.txt");
+	ifstream archivoEntrada("input/inputFile.txt");
+	ofstream archivoSalida("output/outputFile.txt");
 	
-	if(archivoEntrada.is_open()){
+	if(archivoEntrada.is_open() && archivoSalida.is_open()){
        cout<<"archivo abierto "<< endl;
        string linea;
        
        while(getline(archivoEntrada , linea )){
-       
+       archivoSalida << linea;
        
                                     
-       }                                                          
+       }                      
+	   cout<<"Salida exitosa"<<endl;                                    
     }
 }
